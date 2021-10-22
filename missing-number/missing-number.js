d1d7da2fc6106bcd20e04f3a638f1df1caa16ae0
Grant Taggart <granttaggartsf@gmail.com>
2021-10-22 11:18:09 -0700

    Time: 172 ms (12.87%), Space: 41.1 MB (61.77%) - LeetHub

--- missing-number/missing-number.js
+++ missing-number/missing-number.js
@@ -3,11 +3,14 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    let sorted = nums.sort((a, b) => a- b);
     let n = nums.length;
     
     for (let i = 0; i <= n; i++) {
-        if (nums.indexOf(i) === -1) {
+        let num = sorted[i]
+        if (num !== i) {
             return i;
         }
     }
+   
 };
