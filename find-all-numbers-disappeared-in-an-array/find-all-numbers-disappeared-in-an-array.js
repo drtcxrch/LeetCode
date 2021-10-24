2a9572d489384f53a2003379a6a20b5aabceeda8
Grant Taggart <granttaggartsf@gmail.com>
2021-10-24 11:38:57 -0700

    Time: 221 ms (30.82%), Space: 48.9 MB (52.61%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,17 +3,24 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    let sorted = nums.sort();
+    let sorted = nums.sort((a, b) => a - b);
+    let missing = [];
     let i = 1;
     let n = nums.length;
-    let missing = [];
+    let pointer = 0;
     
-    while (i <= n) {
-        if (nums.indexOf(i) === -1) {
-            missing.push(i)
+    while (i <= n) { //i = 1, [1, 2, 2, 3, 3, 4, 7, 8]
+        while(sorted[pointer] === i) { 
+            pointer++;
+        }
+        
+        if (i !== sorted[pointer - 1]) {
+            missing.push(i);
         }
         i++;
     }
+
+    
     
     return missing;
 };
