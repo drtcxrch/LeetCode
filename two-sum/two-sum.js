66c61470c452d0bb7b2df4a04074d05e932cd35b
Grant Taggart <granttaggartsf@gmail.com>
2021-11-30 10:50:57 -0800

    Time: 112 ms (48.99%), Space: 39.4 MB (86.96%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,16 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var output = [];
+    let indices = [];
     
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                output.push(i, j);
+    for (let i = 0; i < nums.length; i++) {
+        let num1 = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            let num2 = nums[j];
+            if (num1 + num2 === target) {
+                indices.push(i);
+                indices.push(j);
             }
         }
     }
     
-    return output;
-    
+    return indices;
 };
