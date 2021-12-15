46dc00e219bf0f87ef5e71943148b3664c50c4fc
Grant Taggart <granttaggartsf@gmail.com>
2021-12-14 17:17:30 -0800

    Time: 119 ms (5.67%), Space: 38.2 MB (88.67%) - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let dp = [1, 1];
+ let dp = [1, 1];
     
-    for (let i = 2; i <= n; i++) {
-        let steps = dp[0] + dp[1];
-        dp[0] = dp[1];
-        dp[1] = steps;
+    for (let i = 1; i <= n; i++) {
+        let temp = dp[1]
+        dp[1] = dp[0] + dp[1];
+        dp[0] = temp;
     }
     
-    return dp[1];
+    return dp[0]
+    
 };
