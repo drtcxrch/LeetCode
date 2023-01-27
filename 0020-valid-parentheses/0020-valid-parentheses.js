91a27201f58cb2baea793176ef2195c5d4923b4c
Grant Taggart <granttaggartsf@gmail.com>
2023-01-26 16:33:25 -0800

    Time: 73 ms (63.73%), Space: 42.5 MB (51.41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let stack = [];
+  
+  for (let i = 0; i < s.length; i++) {
+      let char = stack[stack.length - 1];
+
+      if ((s[i] === '(') || (s[i] === '[') || (s[i] === '{')) {
+          stack.push(s[i]);
+      } else if ((char === '(' && s[i] === ')') || (char === '[' && s[i] === ']') || (char === '{' && s[i] === '}')) {
+          stack.pop();
+      } else {
+          return false;
+      }
+  }
+
+  return stack.length ? false : true;
+};
