9fd6b6c68470eaa0a53281fdd5344d014530b746
Grant Taggart <granttaggartsf@gmail.com>
2023-02-26 13:40:53 -0800

    Time: 64 ms (94.75%), Space: 44 MB (19.84%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -5,16 +5,14 @@
 var titleToNumber = function(columnTitle) {
     let indices = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let index = 0;
-    let outOf = 26;
+    let outOf = 1;
     
    for (let i = columnTitle.length - 1; i >= 0; i--) {
        let char = columnTitle[i];
-       if (i === columnTitle.length - 1) {
-           index += (indices.indexOf(char) + 1)
-       } else {
+      
            index += ((indices.indexOf(char) + 1) * outOf);
            outOf *= 26;
-       }
+       
        
    }
     
