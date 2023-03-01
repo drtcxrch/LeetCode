e09eed76548cc58a9f1f86ff2f3ebc99ec8a9733
Grant Taggart <granttaggartsf@gmail.com>
2023-02-28 17:25:07 -0800

    Time: 96 ms (51.77%), Space: 47.8 MB (31.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,21 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
-    }
-    
-    let sortedS = s.split('').sort();
-    let sortedT = t.split('').sort();
-    let i = 0;
-    
-    while (i < s.length) {
-        if (sortedS[i] !== sortedT[i]) {
-            return false;
-        }
-        
-        i++;
-    }
-    
-    return true;
+    return s.split('').sort().join() === t.split('').sort().join();   
 }
