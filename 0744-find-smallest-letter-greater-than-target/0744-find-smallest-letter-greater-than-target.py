841d9d8285084e39377fe7b1bcd9e1e380e3e214
Grant Taggart <granttaggartsf@gmail.com>
2023-05-01 13:13:08 -0700

    Time: 131 ms (5.07%), Space: 16.5 MB (5.39%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -1,10 +1,10 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        alpha = 'abcdefghijklmnopqrstuvwxyz'
-        
+ 
         for char in letters:
-            if alpha.index(char) > alpha.index(target):
+            if char > target:
                 return char
         
         return letters[0]
+       
         
