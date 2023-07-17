73b181c36dfb1926a5c376d456fb7dc1e2c14ee9
Grant Taggart <granttaggartsf@gmail.com>
2023-07-17 15:15:09 -0700

    Time: 379 ms (14.40%), Space: 44.2 MB (77.80%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length;
+    
+    for (let i = 0; i <= n; i++) {
+        if (nums.indexOf(i) === -1) {
+            return i
+        }
+    }
+};
