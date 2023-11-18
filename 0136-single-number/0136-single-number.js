4cfd21194673e7997fa2bf5f5a248f7895c28bef
Grant Taggart <granttaggartsf@gmail.com>
2023-11-18 09:53:45 -0800

    Time: 67 ms (42.16%), Space: 48.2 MB (14.74%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,18 +4,14 @@
  */
 var singleNumber = function(nums) {
     let seen = {};
-    let sum = 0;
-    
     for (let num of nums) {
         let numStr = String(num);
-            if (!seen[numStr]) {
-                seen[numStr] = true;
-                sum += num
-            } else {
-                delete seen[numStr];
-                sum -= num;
-            }
+        if (!seen[numStr]) {
+            seen[numStr] = true;
+        } else {
+            delete seen[numStr];
+        }
     }
     
-    return sum;
+    return Number(Object.keys(seen)[0])
 };
