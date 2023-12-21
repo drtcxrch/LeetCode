b89dca3b928edcf201a782d1156eec09cb71264a
Grant Taggart <granttaggartsf@gmail.com>
2023-12-21 07:16:06 -0800

    Time: 54 ms (36.74%), Space: 42 MB (43.22%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let node = head;
+    let count = 0;
+    
+    while (node) {
+        count++;
+        node = node.next
+    }
+    
+    node = head;
+    
+    let mid = Math.floor(count / 2);
+    console.log(mid)
+    while (node) {
+        
+        if (mid === 0) {
+            return node;
+        }
+       mid--;
+        node = node.next;
+    }
+};
