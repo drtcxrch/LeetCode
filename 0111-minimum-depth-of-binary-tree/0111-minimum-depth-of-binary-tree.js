0ffa2cc90e71fde1c6603d425581d0bd98b683c0
Grant Taggart <granttaggartsf@gmail.com>
2024-03-03 09:19:37 -0800

    Time: 227 ms (11.89%), Space: 86.7 MB (56.96%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    let min;
+    
+    if (!root) {
+        return 0;
+    }
+    
+    var traverseTree = function(node, level) {
+        if (!node) {
+            return;
+        }
+        
+        if (!node.left && !node.right && (!min || level < min)) {
+            min = level;
+        }
+        
+        let newLevel = level + 1;
+        
+        traverseTree(node.left, newLevel);
+        traverseTree(node.right, newLevel);
+    }
+    
+    traverseTree(root, 1)
+    
+    return min;
+};
