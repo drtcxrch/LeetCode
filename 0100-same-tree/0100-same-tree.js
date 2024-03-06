8a6b6890725a6f073d2c5d1c23745f88c0ee61c5
Grant Taggart <granttaggartsf@gmail.com>
2024-03-06 13:46:38 -0800

    Time: 52 ms (56.76%), Space: 49 MB (62.29%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    const pString = JSON.stringify(p);
+    const qString = JSON.stringify(q);
+    
+    return pString === qString;
+    
+};
